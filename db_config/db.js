03829bb8269481532245d3e110138ff397296868
rock-salt-poison <rock.salt.poison@gmail.com>
2024-11-23 11:39:38 +0530

    Updates

--- db_config/db.js
+++ db_config/db.js
@@ -4,14 +4,17 @@ require('dotenv').config();
 // Function to get database configuration
 function getDbConfig(dbName = 'AmbrsariyaMall') {
   return {
-    user: process.env.DB_USER || 'Merchant',          // Default to 'Merchant' if not set
-    host: process.env.DB_HOST || 'localhost',         // Default to 'localhost'
-    password: process.env.DB_PASSWORD || 'merchant123', // Default password
-    database: process.env.DB_NAME || dbName,          // Default database name
-    port: parseInt(process.env.DB_PORT, 10) || 5432,  // Default port 5432
-    ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false, // SSL for production
+    // user: process.env.DB_USER || 'Merchant',          // Default to 'Merchant' if not set
+    // host: process.env.DB_HOST || 'localhost',         // Default to 'localhost'
+    // password: process.env.DB_PASSWORD || 'merchant123', // Default password
+    // database: process.env.DB_NAME || dbName,          // Default database name
+    // port: parseInt(process.env.DB_PORT, 10) || 5432,  // Default port 5432
+    // ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false, // SSL for production
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
   };
 }
+
 
 // Function to create a new connection pool
 function createDbPool(dbName) {
